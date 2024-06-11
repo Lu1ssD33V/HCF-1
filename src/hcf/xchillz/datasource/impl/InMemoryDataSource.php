0db2ad8147db8912f546bb9c3d00f43e3f4cbc7b
xchillz <cendaaal@tutanota.com>
2024-06-11 11:15:57 -0400

    feat(data): in-memory datasource methods

--- src/hcf/xchillz/datasource/impl/InMemoryDataSource.php
+++ src/hcf/xchillz/datasource/impl/InMemoryDataSource.php
@@ -6,4 +6,38 @@ namespace hcf\xchillz\datasource\impl;
 
 use hcf\xchillz\datasource\IDataSource;
 
-final class InMemoryDataSource implements IDataSource {}
+final class InMemoryDataSource implements IDataSource {
+
+    const PROFILE_TABLE = 0;
+
+    private $data = [];
+
+    public function add($table, $key, $value) {
+        $this->data[$table][$key] = $value;
+    }
+
+    public function update($table, $id, $key, $value): bool
+    {
+        if (!isset($this->data[$table][$id])) return false;
+
+        $this->data[$table][$id][$key] = $value;
+
+        return true;
+    }
+
+    public function find($table, $key)
+    {
+        return $this->data[$table][$key] ?? null;
+    }
+
+    public function findAll($table)
+    {
+        return $this->data[$table];
+    }
+
+    public function delete($table, $key)
+    {
+        unset($this->data[$table][$key]);
+    }
+
+}
